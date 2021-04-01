1246aae4a72d12c30c5ef68e34ffe14422ab3de0
Mitch <Mitchelbunk@gmail.com>
2021-04-01 09:46:42 +0200

    JS imageslider

--- tests/test10.js
+++ tests/test10.js
@@ -1,31 +1,7 @@
-let slides = document.querySelector(".slider-items").children;
-  let  nextSlide=document.querySelector(".right-slide");
-let  prevSlide=document.querySelector(".left-slide");
-let totalSlides=slides.length;
-let index =0;
+let pictures = [
+    "image1",
+    "image2",
+    "image3",
+];
 
-nextSlide.onclick=function (){
-    next("next");
-}
-
-prevSlide.onclick=function (){
-    next("prev");
-}
-
-function next(direction){
-
-    if (direction == "next") {
-        index++;
-        if (index==totalSlides){
-            index=0;
-        }
-    }
-
-
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].classList.add("active");
-    }
-
-    slides[index].classList.remove("active");
-
-}
+let buttons = document.getElementById('.btn')
